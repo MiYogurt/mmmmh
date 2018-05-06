ad4f48ba5ede81c6fd48e1daa3ab09d78a951c69
ChenMingMing <belovedyogurt@gmail.com>
2018-05-06 15:07:32 +0800

    💔 remove auto updater

--- forge.config.js
+++ forge.config.js
@@ -1,36 +1,36 @@
 module.exports = {
-    "make_targets": {
-        "win32": [],
-        "darwin": ["dmg"],
-        "linux": []
+    make_targets: {
+        win32: [],
+        darwin: ['dmg'],
+        linux: []
     },
-    "electronPackagerConfig": {
-        "packageManager": "yarn",
-        "protocol": "mmmmh://",
-        "icon": "./icon/app.png"
+    electronPackagerConfig: {
+        packageManager: 'yarn',
+        protocol: 'mmmmh://',
+        icon: './icon/app.png'
     },
-    "electronInstallerDMG": {
-        "icon": "./icon/app.icns",
-        "format": "ULFO"
+    electronInstallerDMG: {
+        icon: './icon/app.icns',
+        format: 'ULFO'
     },
-    "electronWinstallerConfig": {
-        "name": "mmmmh"
+    electronWinstallerConfig: {
+        name: 'mmmmh'
     },
-    "electronInstallerDebian": {},
-    "electronInstallerRedhat": {},
-    "github_repository": {
-        "owner": "MiYogurt",
-        "name": "mmmmh",
-        "options": {
+    electronInstallerDebian: {},
+    electronInstallerRedhat: {},
+    github_repository: {
+        owner: 'MiYogurt',
+        name: 'mmmmh',
+        options: {
             protocol: 'https',
             host: 'api.github.com'
         }
     },
-    "publish_targets": {
-        "darwin": ["github"]
+    publish_targets: {
+        darwin: ['github']
     },
-    "windowsStoreConfig": {
-        "packageName": "mmmmh",
-        "name": "mmmmh"
+    windowsStoreConfig: {
+        packageName: 'mmmmh',
+        name: 'mmmmh'
     }
 }
--- src/index.js
+++ src/index.js
@@ -23,35 +23,6 @@ if (is.development) {
   unhandled()
 }
 
-if (!is.development) {
-  const server = 'https://hazel-server-neeokqdvgq.now.sh'
-  const feed = `${server}/update/${process.platform}/${app.getVersion()}`
-  autoUpdater.setFeedURL(feed)
-  setInterval(() => {
-    autoUpdater.checkForUpdates()
-  }, 60000)
-
-  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-    const dialogOpts = {
-      type: 'info',
-      buttons: ['Restart', 'Later'],
-      title: 'Application Update',
-      message: process.platform === 'win32' ? releaseNotes : releaseName,
-      detail:
-        'A new version has been downloaded. Restart the application to apply the updates.'
-    }
-
-    dialog.showMessageBox(dialogOpts, response => {
-      if (response === 0) autoUpdater.quitAndInstall()
-    })
-  })
-
-  autoUpdater.on('error', message => {
-    console.error('There was a problem updating the application')
-    console.error(message)
-  })
-}
-
 const menubar = require('menubar')
 
 const log = (...args) => {
